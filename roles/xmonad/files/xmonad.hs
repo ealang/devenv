aecf66c7680e47d8ce05974ea5cbfc8de1e7045d
eric <ealang@gmail.com>
2016-09-24 18:59:30 -0500

    Bind volume keys in xmonad

--- roles/xmonad/files/xmonad.hs
+++ roles/xmonad/files/xmonad.hs
@@ -1,3 +1,13 @@
 import XMonad
 import XMonad.Hooks.SetWMName
-main = xmonad $ defaultConfig { startupHook = setWMName "LG3D" }
+import XMonad.Util.EZConfig
+import Graphics.X11.ExtraTypes.XF86
+
+main = xmonad $ defaultConfig {
+    startupHook = setWMName "LG3D"
+} `additionalKeys`
+    [
+        ((0, xF86XK_AudioLowerVolume   ), spawn "amixer -D pulse sset Master 5%-"),
+        ((0, xF86XK_AudioRaiseVolume   ), spawn "amixer -D pulse sset Master 5%+"),
+        ((0, xF86XK_AudioMute          ), spawn "amixer -D pulse sset Master toggle")
+    ]
