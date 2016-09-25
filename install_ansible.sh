561072e67dc4dc6d10397155aee5fc25155d4ccb
root <ealang@gmail.com>
2016-09-25 05:29:10 +0000

    Add other install dependencies

--- install_ansible.sh
+++ install_ansible.sh
@@ -1,2 +1,2 @@
-apt-get install -y libffi-dev python-dev python-markupsafe libssl-dev
-pip install ansible==2.0.2.0
+apt-get install -y libffi-dev python-dev python-markupsafe libssl-dev python-pip
+pip install ansible==2.0.2.0 setuptools==27.3.0
